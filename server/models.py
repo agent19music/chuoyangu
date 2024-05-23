dfbdf1785637191fd22b9d0801509f793339360e
agent19music <seanmotanya@gmail.com>
2024-05-23 16:39:07 +0300

    Add marketplace context

--- server/models.py
+++ server/models.py
@@ -13,7 +13,6 @@ db = SQLAlchemy(metadata=metadata)
 
 class Users(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
