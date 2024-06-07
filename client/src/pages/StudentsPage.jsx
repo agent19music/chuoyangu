e5c19b2b55d3652d0db265eb4a3358ac6e7dbb5d
agent19music <seanmotanya@gmail.com>
2024-06-07 17:04:43 +0300

    Update student component

--- client/src/pages/StudentsPage.jsx
+++ client/src/pages/StudentsPage.jsx
@@ -61,7 +61,6 @@ const {students, setCategory} = useContext(StudentContext)
             <div key={student.id} className="col-md-6 mb-3">
               <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                 <div className="col p-4 d-flex flex-column position-static">
-                  <strong className="d-inline-block mb-2 text-primary">ID: {student.id}</strong>
                   <h3 className="mb-0">{student.username}</h3>
                   <div className="mb-1 text-muted">Course: {student.category}</div>
                 </div>
